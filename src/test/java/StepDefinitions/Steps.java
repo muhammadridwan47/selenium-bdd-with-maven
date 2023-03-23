7aef463bb688f49c8baf937adbc6af7d0f35eed5
Muhammad Riduwan <muhammadriduwan4747@gmail.com>
2023-03-23 19:29:44 +0700

    ddd

--- src/test/java/StepDefinitions/Steps.java
+++ src/test/java/StepDefinitions/Steps.java
@@ -4,8 +4,6 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Steps {
@@ -33,7 +31,7 @@ public class Steps {
 
     @Given("the user is on login page")
     public void the_user_is_on_login_page() {
-        driver.get("https://www.facebook.com/");
+//        driver.get("https://www.facebook.com/");
       System.out.println("the_user_is_on_login_page");
     }
     @When("the user enters valid credentials")
