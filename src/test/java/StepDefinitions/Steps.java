8903eedeb5048960a3ad2c4231a4bf388853d57a
Muhammad Riduwan <muhammadriduwan4747@gmail.com>
2023-03-23 19:03:55 +0700

    test

--- src/test/java/StepDefinitions/Steps.java
+++ src/test/java/StepDefinitions/Steps.java
@@ -1,11 +1,32 @@
 package StepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Steps {
+    WebDriver driver;
+
+    // LifeCycle Testing
+    @Before //before Scenario
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options.addArguments("--remote-allow-origins=*"));
+        System.out.println("Hello setup");
+    }
+
+    @After //After Scenario
+    public void teardown(){
+        System.out.println("Hello After");
+        driver.quit();
+    }
 
     @Given("the user is on login page")
     public void the_user_is_on_login_page() {
+        driver.get("https://www.facebook.com/");
       System.out.println("the_user_is_on_login_page");
     }
     @When("the user enters valid credentials")
