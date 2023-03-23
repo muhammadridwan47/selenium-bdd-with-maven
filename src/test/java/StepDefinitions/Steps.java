2fce370351617800a27dd112d48d1d3c87e777ef
Muhammad Riduwan <muhammadriduwan4747@gmail.com>
2023-03-23 19:54:27 +0700

    dddd

--- src/test/java/StepDefinitions/Steps.java
+++ src/test/java/StepDefinitions/Steps.java
@@ -4,23 +4,30 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Steps {
     WebDriver driver;
 
-    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker()
-            .enableVnc().enableRecording();
-
     // LifeCycle Testing
-    @Before //before Scenario
+//    @Before //before Scenario
+//    public void setup() {
+//        driver = wdm.create();
+////        ChromeOptions options = new ChromeOptions();
+////        driver = new ChromeDriver(options.addArguments("--remote-allow-origins=*"));
+////        driver = new ChromeDriver();
+//        System.out.println("Hello setup");
+//    }
+
+    @Before
     public void setup() {
-        driver = wdm.create();
-//        ChromeOptions options = new ChromeOptions();
-//        driver = new ChromeDriver(options.addArguments("--remote-allow-origins=*"));
-//        driver = new ChromeDriver();
-        System.out.println("Hello setup");
+        System.setProperty("webdriver.chrome.driver", "linuxchromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless";
+        driver = new ChromeDriver(options);
     }
+
 
     @After //After Scenario
     public void teardown(){
@@ -30,7 +37,7 @@ public class Steps {
 
     @Given("the user is on login page")
     public void the_user_is_on_login_page() {
-//        driver.get("https://www.facebook.com/");
+      driver.get("https://www.facebook.com/");
       System.out.println("the_user_is_on_login_page");
     }
     @When("the user enters valid credentials")
