a64e492fbe7ebb66f4ad515caa6d32ef0b46c6e6
Muhammad Riduwan <muhammadriduwan4747@gmail.com>
2023-03-23 17:11:45 +0700

    test

--- src/test/java/StepDefinitions/Steps.java
+++ src/test/java/StepDefinitions/Steps.java
@@ -1,47 +1,24 @@
 package StepDefinitions;
 
 import io.cucumber.java.en.*;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.concurrent.TimeUnit;
 
 public class Steps {
-    ChromeOptions options = new ChromeOptions();
-    WebDriver driver = new ChromeDriver(options.addArguments("--remote-allow-origins=*"));
+
     @Given("the user is on login page")
     public void the_user_is_on_login_page() {
-        driver.get("http://barru.pythonanywhere.com/daftar");
+      System.out.println("the_user_is_on_login_page");
     }
     @When("the user enters valid credentials")
     public void the_user_enters_valid_credentials() {
-        driver.findElement(By.xpath("/html/body/div/div[2]/form/input[1]")).sendKeys("tester@jagoqa.com"); // input email
-        driver.findElement(By.cssSelector("input#password")).sendKeys("testerjago"); // input password
+        System.out.println("the_user_enters_valid_credentials");
     }
     @When("hits submit")
     public void hits_submit() {
-        driver.findElement(By.id("signin_login")).click(); // click the button
+        System.out.println("hits_submit");
     }
     @Then("the user should be logged in successfully")
     public void the_user_should_be_logged_in_successfully() {
-        try {
-            // validation
-            TimeUnit.SECONDS.sleep(2);
-            String notificationTitle = driver.findElement(By.id("swal2-title")).getText();
-            String notificationMessage = driver.findElement(By.id("swal2-content")).getText();
-            //expectation
-            Assert.assertTrue(notificationTitle.equals("Welcome tester jago"));
-            Assert.assertTrue(notificationMessage.equals("Anda Berhasil Login"));
-            TimeUnit.SECONDS.sleep(2); // this using tryCatch handle
-            driver.quit();
-        }
-        catch (InterruptedException | Error e) {
-            if(e instanceof Error) System.out.println(((Error) e).getMessage());
-            if(e instanceof InterruptedException) System.err.format("IOException: %s%n", e);
-        }
+        System.out.println("the_user_should_be_logged_in_successfully");
     }
 
 }
